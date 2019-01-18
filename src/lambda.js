1662efc5e4e22ff765c527c4718b976c585a15f7
Tyler Long <tyler4long@gmail.com>
2019-01-18 13:13:24 +0800

    Fix lambda async issue

--- src/lambda.js
+++ src/lambda.js
@@ -1,4 +1,12 @@
 import app from './app'
 import serverlessHTTP from 'serverless-http'
+import delay from 'timeout-as-promise'
 
-module.exports.app = serverlessHTTP(app)
+const handler = serverlessHTTP(app)
+
+module.exports.app = async (event, context) => {
+  const result = await handler(event, context)
+  console.log(event, result)
+  await delay(3000) // hacky
+  return result
+}
